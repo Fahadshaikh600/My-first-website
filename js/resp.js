e8c2fe88adf730449c6375b2b5d3e9f47f3727a5
Fahad <fahadshaikh6000@gmail.com>
2022-06-20 20:34:23 +0500

    Solved a error 1

--- js/resp.js
+++ js/resp.js
@@ -2,12 +2,10 @@ burger = document.querySelector(".burger");
 rightNav = document.getElementById("right-nav");
 navList = document.getElementById("nav-list");
 navbar = document.getElementById("navbar");
-content = document.querySelectorAll(".content");
+// content = document.querySelectorAll(".content");
 
 burger.addEventListener("click", () => {
   navbar.classList.toggle("nav-h-resp");
   rightNav.classList.toggle("v-class-resp");
   navList.classList.toggle("v-class-resp");
 });
-
-content.classList.toggle("txt-ceter");
